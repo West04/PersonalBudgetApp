a3b501e4be5741a63477b678e0e3f001c5c0372f
West04 <west@finleysound.com>
2025-10-15 11:39:33 -0700

    adding: all routers in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
-from .routers import categories
+from .routers import categories, budgets, transactions
 
 
 app = FastAPI()
 
 app.include_router(categories.router)
+app.include_router(budgets.router)
+app.include_router(transactions.router)
